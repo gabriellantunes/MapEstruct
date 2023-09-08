f151654e49167c867d165700e66809504f1685f8
Gabriel Antunes <106894238+gabriellantunes@users.noreply.github.com>
2023-09-08 09:55:58 -0300

    Update Main.java

--- estruturaMapa/src/main/java/br/com/meuprojeto/Main.java
+++ estruturaMapa/src/main/java/br/com/meuprojeto/Main.java
@@ -5,10 +5,10 @@ public class Main {
         Mapa<String, Integer> mapa = new Mapa<>();
         mapa.colocar("um", 1);
         mapa.colocar("dois", 2);
-        System.out.println(mapa.obter("um"));  // Deve imprimir 1
-        System.out.println(mapa.obter("dois"));  // Deve imprimir 2
+        System.out.println(mapa.obter("um"));
+        System.out.println(mapa.obter("dois"));
 
         mapa.remover("um");
-        System.out.println(mapa.obter("um"));  // Deve imprimir null
+        System.out.println(mapa.obter("um"));
     }
 }
